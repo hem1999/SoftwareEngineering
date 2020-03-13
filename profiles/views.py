1f4d702b1859d5fbd1e38b1b10fd0e793c923df9
hem1999 <ayyappahemanth1@gmail.com>
2020-03-13 17:32:46 +0530

    Requirements added

---
+++ profiles/views.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from bs4 import BeautifulSoup as Bsoup 
+import requests
+import urllib3
+import scholarly
+import re
+# Create your views here.
+def searchAuthors(request):
+	
+
+	keyword = request.POST.get('keyword',False)
+	Authors =[]
+	Authorlink = []
+	url = 'http://scholar.google.com/citations?view_op=search_authors&mauthors='+str(keyword)+'&hl=en&oi=drw'
+	response = requests.get(url)
+	
+
+	html_soup = Bsoup(response.text,'html.parser')
+
+
+
+	name_containers = html_soup.find_all('div',class_='gsc_1usr')
+
+	interests = []
+
+	for container in name_containers:
+		interest = container.find_all('a',class_ = 'gs_ai_one_int')
+		mylist = []
+		for i in interest:
+			mylist.append(i.text)
+		interests.append(mylist)
+		Authors.append(container.div.h3.a.text)
+		k = str(container.div.h3.a['href'])
+		Authorlink.append("https://scholar.google.com/citations?view_op=medium_photo&user="+k[k.index('='):])
+	author = zip(Authors, Authorlink,interests)
+
+
+
+
+
+	return render(request,'profiles/index.html',context= {'author':author})
+
+
+
+def SingleAuthor(request,authorName):
+
+
+
+	search_query = scholarly.search_author(authorName)
+	author = next(search_query).fill()
+	publicationlist = enumerate([pub.bib['title'] for pub in author.publications])
+	context ={
+		'author':author,
+		'publicationlist':publicationlist,
+	}
+
+	return render(request,'profiles/author.html',context)
+	
+def SinglePublication(request,authorName,pubind):
+
+	search_query = scholarly.search_author(authorName)
+	author = next(search_query).fill()
+	myPublication = author.publications[pubind].fill()
+	pages = None 
+	volume = None
+	journal = None
+	eprint = None
+	originalUrl=None
+	year = None
+	context = {}
+	
+	title = myPublication.bib['title']
+	abstract = re.sub('<[^>]+>', '', str(myPublication.bib['abstract']))
+	author = myPublication.bib['author']
+	if 'eprint' in myPublication.bib.keys():
+		eprint = myPublication.bib['eprint']
+	if 'journal' in myPublication.bib.keys():
+		journal = myPublication.bib['journal']
+	if 'pages' in myPublication.bib.keys():
+		pages = myPublication.bib['pages']
+	if 'originalUrl' in myPublication.bib.keys():
+		originalUrl = myPublication.bib['url']
+	if 'volume' in myPublication.bib.keys():
+		volume = myPublication.bib['volume']
+	if 'year' in myPublication.bib.keys():
+		year = myPublication.bib['year']
+	citedby = myPublication.citedby
+	idCitations = myPublication.id_citations
+	idScholarcitedby = myPublication.id_scholarcitedby
+
+
+	
+	context['authorName']=authorName,
+	context['author']=author,
+	context['pubind']=pubind,
+	context['title']=title,
+	context['abstract']=abstract,
+	context['pages']=pages
+	context['eprint']=eprint,
+	context['journal']=journal,
+	context['originalUrl']=originalUrl,
+	context['volume']=volume
+	context['year']=year,
+	context['citedby']=citedby,
+	context['idCitations']=idCitations,
+	context['idScholarcitedby']=idScholarcitedby
+	return render(request,'profiles/publication.html',context)
